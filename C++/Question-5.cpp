3a764ce33528cdb6807958ae1d66e3cb5717fb38
Akshaygupta-09 <akshaykrgupta7@gmail.com>
2024-05-04 11:47:41 +0530

    done

--- C++/Question-5.cpp
+++ C++/Question-5.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 
 // Function to calculate the factorial of a number
-int factorial(int n) {
-        return n * factorial(n - 1);
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
 
-int main() {
+int main()
+{
     int number;
-    
+
     std::cout << "Enter a number: ";
     std::cin >> number;
-    
+    if(number<0) {
+        std::cout << "The factorial of " << number << " not exist " <<std::endl;
+    }
+    else{
     int result = factorial(number);
-    
     std::cout << "The factorial of " << number << " is " << result << std::endl;
-    
+    }
     return 0;
 }
