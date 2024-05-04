1ae91823f3d720208d6d6881314e4b0feacff12c
Amaanhussain11 <123290835+Amaanhussain11@users.noreply.github.com>
2024-05-04 10:24:19 +0530

    Update Question-3.py

--- Python/Question-3.py
+++ Python/Question-3.py
@@ -5,9 +5,9 @@ def guess_number():
     number = random.randint(1, 100)
     while True:
         guess = int(input("Guess the number (between 1 and 100): "))
-        if guess < number:
+        if guess > number:
             print("Too low! Try again.")
-        elif guess > number:
+        elif guess <= number:
             print("Too high! Try again.")
         else:
             print("Congratulations! You guessed the number.")
