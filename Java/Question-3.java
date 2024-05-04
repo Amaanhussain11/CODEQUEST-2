315d9beba2cc9d3b0d06e8cfac13623ed31b3de4
Amaanhussain11 <123290835+Amaanhussain11@users.noreply.github.com>
2024-05-04 10:22:10 +0530

    Update Question-3.java

--- Java/Question-3.java
+++ Java/Question-3.java
@@ -4,16 +4,16 @@ import java.util.Random;
 public class NumberGuessingGame {
     public static void guessNumber() {
         Random random = new Random();
-        int number = random.nextInt(100) + 1; // Generates a random number between 1 and 100
+        int number = random.nextInt(100) + 1;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("Guess the number (between 1 and 100): ");
             int guess = scanner.nextInt();
 
-            if (guess < number) {
+            if (guess > number) {
                 System.out.println("Too low! Try again.");
-            } else if (guess > number) {
+            } else if (guess <= number) {
                 System.out.println("Too high! Try again.");
             } else {
                 System.out.println("Congratulations! You guessed the number.");
