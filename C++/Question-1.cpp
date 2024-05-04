cd52e79f0d09e057dfc414b84e003efd6c37d364
Krishna <krishnapodaiya@gmail.com>
2024-05-04 12:20:18 +0530

    done

--- C++/Question-1.cpp
+++ C++/Question-1.cpp
@@ -3,8 +3,8 @@
 
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        for (int j = i; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
+        for (int j = 0; j < n - i - 1; j++) {    //we have to make j=i to j=0
+            if (arr[j] > arr[j + 1]) {  //to make ascending order less than(<) to greater than(>)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
